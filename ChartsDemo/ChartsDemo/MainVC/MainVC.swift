24a1a980159836a85e805d89e629c525b36c4f2e
leoho0722 <leo160918@gmail.com>
2022-04-03 15:26:25 +0800

    First Commit

---
+++ ChartsDemo/ChartsDemo/MainVC/MainVC.swift
@@ -0,0 +1,16 @@
+//
+//  MainVC.swift
+//  ChartsDemo
+//
+//  Created by Leo Ho on 2022/4/3.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(12345678)
+    }
+}
